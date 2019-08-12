96b98cc9af370b8bee05f3409f837cac438dab38
Pranav Singhal <pranavsinghal96@gmail.com>
2019-08-12 11:39:17 +0530

    added apis to retrive blocks and transaction data

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(
 
 
 let db;
-const getBlocks = async (latestIndex,db) => {
+const getBlocks = async (latestIndex, db) => {
     for (let i = 0; i <= 3; i++) {
         await web3.eth.getBlock(latestIndex - i).then(block => {
             let blockObj = {
@@ -32,14 +32,14 @@ const getBlocks = async (latestIndex,db) => {
 MongoClient.connect('mongodb://localhost:27017/block', function (err, client) {
     if (err) throw err;
 
-     db = client.db('blocks');
+    db = client.db('blocks');
     web3.eth.getBlock('latest').then((latestBlock) => {
 
 
         console.log("starting to index blocks");
-        getBlocks(latestBlock.number,db).then(()=> {
+        getBlocks(latestBlock.number, db).then(() => {
             console.log("indexing completed");
-            app.listen(3000, ()=> {
+            app.listen(3000, () => {
                 console.log("'app now listening on port 3000'")
             });
         })
@@ -77,11 +77,38 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/getBlockByHash',function (req,response) {
-    console.log(req.body);
-    db.collection('blocks').findOne(req.body, function (err,res) {
+app.get('/getBlockByNumber/:blockNumber', function (req, response) {
+    req.params.blockNumber = parseInt(req.params.blockNumber);
+    console.log(req.params);
+    db.collection('blocks').findOne(req.params, function (err, res) {
         console.log(res);
+        response.send(res)
+    })
+});
+
+app.get('/getBlockByHash/:blockHash', function (req, response) {
+
+    db.collection('blocks').findOne(req.params, function (err, res) {
+
         response.send(res);
     })
+
+});
+
+
+
+app.get('/getTransactionData/:txHash',function (req,response) {
+    web3.eth.getTransaction(req.params.txHash).then(tx => {
+        // console.log(tx);
+        let resObj  ={};
+        resObj.txHash = req.params.txHash;
+        resObj.from = tx.from;
+        resObj.to = tx.to;
+        resObj.blockNumber = tx.blockNumber;
+        response.send(resObj);
+
+
+    })
 })
+
 
